4cdcd7a3e7d247db33de595c80277564a0b83903
Eric J. Holmes <eric@ejholmes.net>
2012-06-03 12:41:37 -0700

    Add configuration class.

---
+++ lib/dbdc/config.rb
@@ -0,0 +1,52 @@
+module Dbdc
+  class << self
+    attr_writer :log
+
+    # Returns the current Configuration
+    #
+    #    Dbdc.configuration.username = "username"
+    #    Dbdc.configuration.password = "password"
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    # Yields the Configuration
+    #
+    #    Dbdc.configure do |config|
+    #      config.username = "username"
+    #      config.password = "password"
+    #    end
+    def configure
+      yield configuration
+    end
+
+    def log?
+      @log ||= false
+    end
+
+    def log(message)
+      return unless Dbdc.log?
+      Dbdc.configuration.logger.send :debug, message
+    end
+  end
+
+  class Configuration
+    # The username to use during login.
+    attr_accessor :username
+    # The password to use during login.
+    attr_accessor :password
+    # The security token to use during login.
+    attr_accessor :security_token
+    # Set this to true if you're authenticating with a Sandbox instance.
+    # Defaults to false.
+    attr_accessor :host
+
+    def initialize
+      @host ||= 'login.salesforce.com'
+    end
+
+    def logger
+      @logger ||= ::Logger.new STDOUT
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,6 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.cassette_library_dir = 'fixtures/requests'
   config.hook_into :faraday
 end
